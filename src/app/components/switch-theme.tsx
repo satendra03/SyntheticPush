fc4e88161a3339dea60be5f247e2141a2ea68bb9
satendra03 <satendrakumarparteti.work@gmail.com>
2025-07-13 01:31:43 +0530

    created a basic application

---
+++ src/app/components/switch-theme.tsx
@@ -0,0 +1,33 @@
+// src/app/components/switch-theme.tsx
+"use client";
+
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export function SwitchTheme() {
+  const { setTheme, theme } = useTheme();
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button
+          variant="outline"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className="relative"
+        >
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 " />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent className="bg-background text-foreground">
+        {theme === "dark" ? "Light mode" : "Dark mode"}
+      </TooltipContent>
+    </Tooltip>
+  );
+}
