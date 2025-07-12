7f454f0b432457c9df5ba87392bc74d3c410d5bf
satendra03 <satendrakumarparteti.work@gmail.com>
2025-07-13 01:51:47 +0530

    added all variant of button (green, dark, outline, noOutline)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import { SwitchTheme } from "./components/switch-theme";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen gap-2">
+      <Button variant="green">Sign up for GitHub</Button>
       <Button variant="dark">Sign up for GitHub</Button>
+      <Button variant="noOutline">Sign up for GitHub</Button>
+      <Button variant="outline">Sign up for GitHub</Button>
       <SwitchTheme />
     </div>
   );
