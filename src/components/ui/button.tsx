ebe876d8af31b5ad401fcaefec5a0a230de14faf
satendra03 <satendrakumarparteti.work@gmail.com>
2025-07-24 22:42:14 +0530

    fixed button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:text-primary-foreground cursor-pointer hover:bg-primary/90 active:scale-95 transition-all",
   {
     variants: {
       variant: {
@@ -19,8 +19,9 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
-        green: "bg-green-500 text-white shadow-xs hover:bg-green-500/90",
+          link: "text-primary underline-offset-4 hover:underline",
+          green: "bg-green-600 text-white font-bold shadow-xs hover:bg-green-700/90",
+          dark: "bg-gray-600 text-white font-bold shadow-xs hover:bg-gray-800/90 border-2 border-white",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
